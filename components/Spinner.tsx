a61209e910df3bc166d8897842cd43244075fc88
cirogithub1 <cirosequea.fr@gmail.com>
2023-03-07 21:07:11 +0100

    07-03-23

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,6 +1,6 @@
 
 function Spinner({ size }:{ size:string }) {
-	return (
+	return(
 		<div className="flex justify-center items-center py-3">
 			<div className={`animate-spin rounded-full ${size} border-t-2 border-red-700`}/>
 		</div>
